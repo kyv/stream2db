4b158d685fce843d562dc5c2d6603aed2c229246
Timothy Kevin Brown <kevin@ki-ai.org>
2017-12-12 16:25:57 -0600

    use randomized list of urls in compranet updater
      * remove unused vars
      * remove surpluss comments

--- bin/updateProduction.sh
+++ bin/updateProduction.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# MONGO_HOST=
-# MONGO_COLLECTION=
 BACKEND=mongo
 TIMESTAMP=$(date "+%s")
-# DATABASE="compranet-${TIMESTAMP}"
 DATABASE="compranet-intermediary"
 PROXY_URL="https://excel2json.herokuapp.com"
+
 URLS=(
   https://upcp.funcionpublica.gob.mx/descargas/Contratos2017.zip
   https://upcp.funcionpublica.gob.mx/descargas/Contratos2016.zip
@@ -16,7 +14,11 @@ URLS=(
   https://compranetinfo.funcionpublica.gob.mx/descargas/cnet/Contratos2010_2012.zip
 )
 
-for url in "${URLS[@]}";
+# randomize list to increase likely hood of importing new documents on reruns
+IFS=$'\n' SORTED_URLS=($(shuf <<<"${URLS[*]}"))
+unset IFS
+
+for url in "${SORTED_URLS[@]}";
 do
   echo "streaming ${PROXY_URL}/${url} to ${BACKEND}"
   stream2db --backend ${BACKEND} \
@@ -25,9 +27,3 @@ do
     --type compranet \
     ${PROXY_URL}/${url}
 done
-
-# TODO script to loop over compranet docs
-# 1. aggregation
-# group contracts by num_proced and process from there
-# or
-# 2. loop over all num_proced
