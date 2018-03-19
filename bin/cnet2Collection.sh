4ffb688542fe5ebc8acb29e9c85080e4df8a4da1
kyv <kevin@ki-ai.org>
2018-03-19 11:28:56 -0400

    just choose one url instead of looping over all of them

--- bin/cnet2Collection.sh
+++ bin/cnet2Collection.sh
@@ -10,15 +10,12 @@ URLS=(
   https://upcp.funcionpublica.gob.mx/descargas/Contratos2015.zip
 )
 
-# randomize list to increase likely hood of importing new documents on reruns
-IFS=$'\n' SORTED_URLS=($(shuf <<<"${URLS[*]}"))
-unset IFS
+size=${#URLS[@]}
+index=$(($RANDOM % $size))
+url=${URLS[$index]}
 
-for url in "${SORTED_URLS[@]}";
-do
-  printf "streaming ${PROXY_URL}/${url} to ${BACKEND}\n"
-  stream2db --backend ${BACKEND} \
-    --id hash \
-    --type compranet \
-    ${PROXY_URL}/${url}
-done
+printf "streaming ${PROXY_URL}/${url} to ${BACKEND}\n"
+stream2db --backend ${BACKEND} \
+  --id hash \
+  --type compranet \
+  ${PROXY_URL}/${url}
